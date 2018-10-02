a8e024bf3d304252a842de6eeb418ed73b7c9793
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-10-02 15:00:47 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+require('./web.js');
+
+var static = require('node-static');
+var file = new static.Server();
+require('http').createServer(function(request, response) {
+  request.addListener('end', function() {
+    file.serve(request, response);
+  }).resume();
+}).listen(8080);
