11bc39099fb313d1ea4147422986cb35e299478f
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-10-02 15:15:40 +0900

    Update web.js

--- web.js
+++ web.js
@@ -1,1 +1,116 @@
+var express = require('express');
+var bodyParser     =        require("body-parser");
+var app = express();
+var path = require('path');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.post("/v1/users/{username}/link-to-eos-account", function(req, res) { 
+
+	  var id = req.body.id;
+	  var vote = req.body.vote;
+	  console.log("vote event", id, vote);
+	  //save this data to mongoDB//
+	  var body = {
+			  "result": "done",
+			  "balance" : result,
+			  "account" : result2
+	  };
+	  
+	  res.send("done");
+});
+
+app.post("/v1/users/thanks", function(req, res) { 
+
+	  var id = req.body.id;
+	  var vote = req.body.vote;
+	  console.log("vote event", id, vote);
+	  //save this data to mongoDB//
+	  var body = {
+			  "result": "done",
+			  "balance" : result,
+			  "account" : result2
+	  };
+	  
+	  res.send("done");
+});
+
+app.post("/v1/users/transfer", function(req, res) { 
+
+	  var id = req.body.id;
+	  var vote = req.body.vote;
+	  console.log("vote event", id, vote);
+	  //save this data to mongoDB//
+	  var body = {
+			  "result": "done",
+			  "balance" : result,
+			  "account" : result2
+	  };
+	  
+	  res.send("done");
+});
+
+app.post("/v1/users/stake", function(req, res) { 
+
+	  var id = req.body.id;
+	  var vote = req.body.vote;
+	  console.log("vote event", id, vote);
+	  //save this data to mongoDB//
+	  var body = {
+			  "result": "done",
+			  "balance" : result,
+			  "account" : result2
+	  };
+	  
+	  res.send("done");
+});
+
+app.post("/v1/users/unstake", function(req, res) { 
+
+	  var id = req.body.id;
+	  var vote = req.body.vote;
+	  console.log("vote event", id, vote);
+	  //save this data to mongoDB//
+	  var body = {
+			  "result": "done",
+			  "balance" : result,
+			  "account" : result2
+	  };
+	  
+	  res.send("done");
+});
+
+app.get("/v1/users/assets", function(req, res) { 
+
+	  var id = req.body.id;
+	  var vote = req.body.vote;
+	  console.log("vote event", id, vote);
+	  //save this data to mongoDB//
+	  var body = {
+			  "result": "done",
+			  "balance" : result,
+			  "account" : result2
+	  };
+	  
+	  res.send("done");
+});
+
+
+
+
+ /* serves all the static files */
+ app.get(/^(.+)$/, function(req, res){ 
+     console.log('static file request : ' + req.params);
+	 console.log("app get", req.params[0]);
+	 console.log("app get parameter", req.query.name);
+     res.sendfile( __dirname + req.params[0]); 
+ });
+
+ var port = process.env.PORT || 5000;
+console.log("port", port);
+
+ app.listen(port, function() {
+   console.log("Listening on " + port);
+ });
